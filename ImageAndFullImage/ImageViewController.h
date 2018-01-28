b0d2236a38cd488e8b4ac14f2f837185cba488e6
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2018-01-28 17:28:35 -0600

    first commit

---
+++ ImageAndFullImage/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  ImageAndFullImage
+//
+//  Created by Miguel Mexicano on 28/01/18.
+//  Copyright © 2018 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
