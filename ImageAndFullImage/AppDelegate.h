28584c8eabafe5a97c1405c0193f3170916f53c8
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2018-01-28 16:36:44 -0600

    Initial Commit

---
+++ ImageAndFullImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageAndFullImage
+//
+//  Created by Miguel Mexicano on 28/01/18.
+//  Copyright © 2018 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageAndFullImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageAndFullImage
+//
+//  Created by Miguel Mexicano on 28/01/18.
+//  Copyright © 2018 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
